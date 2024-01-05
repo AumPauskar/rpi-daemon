a378469bf841056c7d6a73d2280211532f0dd73a
Aum Pauskar <aumpauksar111@gmail.com>
2024-01-05 10:13:50 +0530

    rgb_control and fan_control added

---
+++ fan_control.py
@@ -0,0 +1,36 @@
+def fan_control():
+    import subprocess
+    import time
+    import RPi.GPIO as GPIO
+    import json
+
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
+        on_temp = data['on-temp']
+        off_temp = data['off-temp']
+        gpio = data['gpio']
+        print(on_temp, off_temp, gpio)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(gpio, GPIO.OUT)
+    fan_on = False
+    while True:
+        temp = subprocess.check_output("vcgencmd measure_temp", shell=True).decode('utf-8')
+        temp_value = float(temp[5:9])
+        
+        if temp_value > on_temp:
+            fan_on = True
+        elif temp_value < off_temp:
+            fan_on = False
+        
+        if fan_on:
+            GPIO.output(gpio, GPIO.HIGH)
+            print("Fan on: ", temp_value)
+        else:
+            GPIO.output(gpio, GPIO.LOW)
+            print("Fan off", temp_value)
+            
+        time.sleep(1)
+	
+if __name__ == "__main__":
+    fan_control()
---
+++ rgb_control.py
@@ -0,0 +1,48 @@
+def rgb_control():
+    """
+    This function turns on the RGB LEDs for 1 second, then turns them off for 1 second.
+    It repeats this process 30 times for now, and can be customised by changing the script
+    """
+    import json
+    import time
+    import RPi.GPIO as GPIO
+    # Specify the path to your JSON file
+    file_path = "config.json"
+
+    # Open the JSON file and load its contents
+    with open(file_path) as file:
+        data = json.load(file)
+        gpio_red = data["gpio-red"]
+        gpio_green = data["gpio-green"]
+        gpio_blue = data["gpio-blue"]
+
+    # Set up GPIO mode and pins
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(gpio_red, GPIO.OUT)
+    GPIO.setup(gpio_green, GPIO.OUT)
+    GPIO.setup(gpio_blue, GPIO.OUT)
+
+    for i in range(30):
+        # Turn on the GPIO pins
+        GPIO.output(gpio_red, GPIO.HIGH)
+        GPIO.output(gpio_green, GPIO.HIGH)
+        GPIO.output(gpio_blue, GPIO.HIGH)
+        print("LEDs on")
+
+        # Wait for 1 second
+        time.sleep(1)
+
+        # Turn off the GPIO pins
+        GPIO.output(gpio_red, GPIO.LOW)
+        GPIO.output(gpio_green, GPIO.LOW)
+        GPIO.output(gpio_blue, GPIO.LOW)
+        print("LEDs off")
+
+        # Wait for 1 second
+        time.sleep(1)
+
+    # Clean up GPIO
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    rgb_control()
